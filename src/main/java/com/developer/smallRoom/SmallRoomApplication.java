84018e1508405f0ac1731a37df20a343ca84e4f6
StartDeveloperKim <ktw550111@naver.com>
2023-06-17 17:31:33 +0900

    Start. 개발자의 작은 방 프로젝트 시작

---
+++ src/main/java/com/developer/smallRoom/SmallRoomApplication.java
@@ -0,0 +1,13 @@
+package com.developer.smallRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/smallRoom/SmallRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.smallRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
