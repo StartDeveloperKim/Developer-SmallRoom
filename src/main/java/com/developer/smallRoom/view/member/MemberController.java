adb7265ddf6cf7d6a56e325a2dd9249e72b254c3
StartDeveloperKim <ktw550111@naver.com>
2023-06-21 20:52:42 +0900

    #코드수정 : 쿠키에 HttpOnly 속성 추가 및 로그아웃 API 추가

--- src/main/java/com/developer/smallRoom/view/member/MemberController.java
+++ src/main/java/com/developer/smallRoom/view/member/MemberController.java
@@ -1,6 +1,11 @@
 package com.developer.smallRoom.view.member;
 
+import com.developer.smallRoom.global.util.CookieUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,4 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        String ACCESS_TOKEN = "access_token";
+        String REFRESH_TOKEN = "refresh_token";
+        CookieUtil.deleteCookie(request, response, ACCESS_TOKEN);
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+
+        SecurityContextHolder.getContext().setAuthentication(null);
+
+        return "redirect:/";
+    }
 }
