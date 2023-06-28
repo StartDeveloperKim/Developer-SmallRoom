59cb329bb42e00dfb132e10399d7ec5d552d3da4
StartDeveloperKim <ktw550111@naver.com>
2023-06-28 15:01:34 +0900

    #테스트코드 : TokenApiController 통합테스트 작성 완료

---
+++ src/test/java/com/developer/smallRoom/view/token/TokenApiControllerTest.java
@@ -0,0 +1,81 @@
+package com.developer.smallRoom.view.token;
+
+import com.developer.smallRoom.application.auth.jwt.JwtProperties;
+import com.developer.smallRoom.application.auth.jwt.TokenProvider;
+import com.developer.smallRoom.application.auth.jwt.refreshToken.RefreshToken;
+import com.developer.smallRoom.application.auth.jwt.refreshToken.RefreshTokenRepository;
+import com.developer.smallRoom.domain.member.Member;
+import com.developer.smallRoom.domain.member.repository.MemberRepository;
+import com.developer.smallRoom.factory.MemberFactory;
+import com.developer.smallRoom.jwt.TestJwtProperties;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Duration;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TokenApiControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+    @Autowired
+    private TokenProvider tokenProvider;
+    @MockBean
+    private JwtProperties jwtProperties;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private String testRefreshToken;
+
+    @BeforeEach
+    void setUp() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        refreshTokenRepository.deleteAll();
+        TestJwtProperties testJwtProperties = new TestJwtProperties();
+        given(jwtProperties.getIssuer()).willReturn(testJwtProperties.getIssuer());
+        given(jwtProperties.getSecretKey()).willReturn(testJwtProperties.getSecretKey());
+
+        Member member = memberRepository.save(MemberFactory.getMemberDefaultValue());
+
+        testRefreshToken = tokenProvider.generateToken(member, Duration.ofDays(14));
+        refreshTokenRepository.save(new RefreshToken(1L, testRefreshToken));
+    }
+
+    @DisplayName("generateNewAccessToken() : 해당 url로 접근하면 RefreshToken 확인한 후 새 AccessToken을 발급한다.")
+    @Test
+    void generateNewAccessToken() throws Exception {
+        //given
+        Cookie refreshTokenCookie = new Cookie("refresh_token", testRefreshToken);
+
+        //when
+        ResultActions result = mvc.perform(get("/api/token")
+                .cookie(refreshTokenCookie));
+        //then
+        result.andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(cookie().exists("access_token"));
+    }
+}
