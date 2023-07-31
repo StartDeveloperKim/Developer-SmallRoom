2de5571560d8744dc41f732690906702e6826736
StartDeveloperKim <ktw550111@naver.com>
2023-07-31 16:26:46 +0900

    #코드변경 : 로컬환경 배포환경 Redis 설정 다르게 구성

--- src/main/java/com/developer/smallRoom/config/RedisProdConfig.java
+++ src/main/java/com/developer/smallRoom/config/RedisProdConfig.java
@@ -1,19 +1,27 @@
 package com.developer.smallRoom.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
 @EnableRedisRepositories
 @PropertySource("classpath:application.properties")
-public class RedisConfig {
+@Profile("prod")
+public class RedisProdConfig {
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -22,7 +30,16 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.clusterNode(host, port);
+        long timeout = 3;
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .clientOptions(ClientOptions.builder()
+                        .socketOptions(SocketOptions.builder()
+                                .connectTimeout(Duration.ofSeconds(timeout)).build())
+                        .build())
+                .commandTimeout(Duration.ofSeconds(timeout)).build();
+        return new LettuceConnectionFactory(clusterConfiguration, clientConfiguration);
     }
 
     @Bean
